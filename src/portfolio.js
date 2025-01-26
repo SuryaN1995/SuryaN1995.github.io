26a2b3c5ad9cd52f2160c2221355b49d51a61709
Surya N <surya.n1447@gmail.com>
2025-01-26 22:09:58 +0530

    Updated the CV v2

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1fyuehO4vGBwL0PfvW7E6f679X7Qhb9YW/view?usp=sharing",
+    "https://drive.google.com/file/d/1iYXCIg2OR1PH9wcviHw5orPr-odlwiJG/view?usp=sharing",
   githubProfile: "https://github.com/SuryaN1995",
 };
 
